c33ad2c797e400b11253aed36a2f1a40cf5aacf0
rickbestgamer <95346478+rickbestgamer@users.noreply.github.com>
2023-09-14 14:59:42 +0200

    main

--- GeldAutomaat/Pages/LandingPage.xaml.cs
+++ GeldAutomaat/Pages/LandingPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,6 +25,30 @@ namespace GeldAutomaat.Pages
         public LandingPage()
         {
             InitializeComponent();
+        }
+
+        private async void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (MainCard.Opacity != 0)
+            {
+                Storyboard storyboard = (Storyboard)FindResource("MainCardAnimationDisapear");
+                Storyboard.SetTarget(storyboard, MainCard);
+                storyboard.Begin();
+                await Task.Delay(500);
+                storyboard = (Storyboard)FindResource("MainCardAnimationApear");
+                Storyboard.SetTarget(storyboard, WelcomeCard);
+                storyboard.Begin();
+            }
+        }
+
+        private void Ellipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Ellipse)sender).Fill = Brushes.Gray;
+        }
+
+        private void Ellipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Ellipse)sender).Fill = Brushes.Black;
         }
     }
 }

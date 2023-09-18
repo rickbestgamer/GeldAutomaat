d69751c82a5f1519668200824ae893f8ed4c63ae
rickbestgamer <95346478+rickbestgamer@users.noreply.github.com>
2023-09-18 15:17:33 +0200

    main

---
+++ GeldAutomaat/Classes/DBData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace GeldAutomaat.Classes
+{
+    internal class DBData
+    {
+        public MySqlConnection connection;
+        readonly string myConnectionString = "server=localhost;uid=root;" +
+                "pwd=;database=geldautomaat";
+        public List<Transactions> transactions = new List<Transactions>();
+        public DBData()
+        {
+            try
+            {
+                connection = new MySqlConnection();
+                connection.ConnectionString = myConnectionString;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public class Transactions
+        {
+            public Transactions() { }
+
+            public int index { get; set; }
+    }
+}
--- GeldAutomaat/Pages/ChoicePage.xaml.cs
+++ GeldAutomaat/Pages/ChoicePage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,27 @@ namespace GeldAutomaat.Pages
     /// </summary>
     public partial class ChoicePage : Page
     {
+        Storyboard MainCardAnimationDisapear;
+        Storyboard MainCardAnimationApear;
         public ChoicePage()
         {
             InitializeComponent();
+            ChoiceCard.Opacity = 0;
+            ChoiceCard.Visibility = Visibility.Visible;
+            MainCardAnimationDisapear = (Storyboard)FindResource("MainCardAnimationDisapear");
+            MainCardAnimationApear = (Storyboard)FindResource("MainCardAnimationApear");
+            Storyboard.SetTarget(MainCardAnimationApear, ChoiceCard);
+            MainCardAnimationApear.Begin();
+        }
+
+        private void Ellipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Ellipse)sender).Fill = Brushes.Gray;
+        }
+
+        private void Ellipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Ellipse)sender).Fill = Brushes.Black;
         }
     }
 }
